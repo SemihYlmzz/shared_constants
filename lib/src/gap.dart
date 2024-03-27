3f6ae5d7248f387e4d6a176ffe2f5dcee46b4fcb
SemihYlmzz <ekkotaymmm@gmail.com>
2024-03-27 23:05:01 +0300

    first commit

---
+++ lib/src/gap.dart
@@ -0,0 +1,13 @@
+import 'package:gap/gap.dart';
+
+class SharedGap {
+  static const Gap gap2 = Gap(2);
+  static const Gap gap4 = Gap(4);
+  static const Gap gap8 = Gap(8);
+  static const Gap gap12 = Gap(12);
+  static const Gap gap16 = Gap(16);
+  static const Gap gap20 = Gap(20);
+  static const Gap gap32 = Gap(32);
+  static const Gap gap64 = Gap(64);
+  static const Gap gap128 = Gap(128);
+}
---
+++ lib/src/src.dart
@@ -0,0 +1,7 @@
+export 'device_sizes.dart';
+export 'durations.dart';
+export 'gap.dart';
+export 'languages.dart';
+export 'messages.dart';
+export 'paddings.dart';
+export 'radius.dart';
