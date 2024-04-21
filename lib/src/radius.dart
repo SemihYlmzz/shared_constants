c798e325a90777b8040a2bdbd5e571485f2c46ad
SemihYlmzz <ekkotaymmm@gmail.com>
2024-04-21 12:14:59 +0300

    Add new BorderRadius constants for top left and top right corners

--- lib/src/radius.dart
+++ lib/src/radius.dart
@@ -6,4 +6,46 @@ class SharedBorderRadius {
   static BorderRadius circular12 = BorderRadius.circular(12);
   static BorderRadius circular16 = BorderRadius.circular(16);
   static BorderRadius circular32 = BorderRadius.circular(32);
+
+  static BorderRadius topLeftRight4 = const BorderRadius.only(
+    topLeft: Radius.circular(4),
+    topRight: Radius.circular(4),
+  );
+  static BorderRadius topLeftRight8 = const BorderRadius.only(
+    topLeft: Radius.circular(8),
+    topRight: Radius.circular(8),
+  );
+  static BorderRadius topLeftRight12 = const BorderRadius.only(
+    topLeft: Radius.circular(12),
+    topRight: Radius.circular(12),
+  );
+  static BorderRadius topLeftRight16 = const BorderRadius.only(
+    topLeft: Radius.circular(16),
+    topRight: Radius.circular(16),
+  );
+  static BorderRadius topLeftRight32 = const BorderRadius.only(
+    topLeft: Radius.circular(32),
+    topRight: Radius.circular(32),
+  );
+
+  static BorderRadius bottomLeftRight4 = const BorderRadius.only(
+    bottomLeft: Radius.circular(4),
+    bottomRight: Radius.circular(4),
+  );
+  static BorderRadius bottomLeftRight8 = const BorderRadius.only(
+    bottomLeft: Radius.circular(8),
+    bottomRight: Radius.circular(8),
+  );
+  static BorderRadius bottomLeftRight12 = const BorderRadius.only(
+    bottomLeft: Radius.circular(12),
+    bottomRight: Radius.circular(12),
+  );
+  static BorderRadius bottomLeftRight16 = const BorderRadius.only(
+    bottomLeft: Radius.circular(16),
+    bottomRight: Radius.circular(16),
+  );
+  static BorderRadius bottomLeftRight32 = const BorderRadius.only(
+    bottomLeft: Radius.circular(32),
+    bottomRight: Radius.circular(32),
+  );
 }
